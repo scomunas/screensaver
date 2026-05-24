e62fea72e7d11ecc3504dba80fb004e17f3a0cf8
Sergio Comuñas <sergio.comunas@gmail.com>
2026-05-24 21:09:10 +0200

    v0.0.3 - Primera versión revisada

--- backend/config.py
+++ backend/config.py
@@ -4,17 +4,19 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Database credentials
-DB_HOST = os.getenv("DB_HOST", "db")
+DB_HOST = os.getenv("DB_IP", "db")
+DB_PORT = int(os.getenv("DB_PORT", 5432))
 DB_NAME = os.getenv("DB_NAME", "screensaver")
 DB_USER = os.getenv("DB_USER", "screensaver_user")
 DB_PASSWORD = os.getenv("DB_PASSWORD", "screensaver_pass")
 
 DB_CONFIG = {
     "host": DB_HOST,
+    "port": DB_PORT,
     "database": DB_NAME,
     "user": DB_USER,
     "password": DB_PASSWORD
 }
 
-# API configuration: Use BACKEND_PORT specifically
-PORT = int(os.getenv("BACKEND_PORT", 9091))
+# API configuration
+PORT = int(os.getenv("BACKEND_PORT", 9090))
